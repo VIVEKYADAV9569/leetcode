efbe3728a6a0311145051f81816ffd8c01a3fe8d
VIVEKYADAV9569 <148001435+VIVEKYADAV9569@users.noreply.github.com>
2025-02-01 16:36:10 +0530

    Time: 0 ms, Memory: 40.5 MB - LeetHub

---
+++ 1054-53-2582-pass-the-pillow/1054-53-2582-pass-the-pillow.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int passThePillow(int n, int time) {
+       int pos=time%(n-1);
+       int dir=time/(n-1);
+       if(dir%2==0){
+        return (1+pos);
+       }
+       else{
+        return n-pos;
+       }
+    }
+}
