5ddb02a6b23d96cd51d0053e8eb2df1dd44243a8
VIVEKYADAV9569 <148001435+VIVEKYADAV9569@users.noreply.github.com>
2025-02-05 15:43:07 +0530

    Time: 2 ms, Memory: 44.8 MB - LeetHub

--- 19105-767-152-maximum-product-subarray/19105-767-152-maximum-product-subarray.java
+++ 19105-767-152-maximum-product-subarray/19105-767-152-maximum-product-subarray.java
@@ -1,28 +1,21 @@
 class Solution {
     public int maxProduct(int[] nums) {
-        if (nums == null || nums.length == 0) {
-            return 0;
+        if(nums.length==0){
+            return 1;
         }
-
-        int maxProduct = nums[0];
-        int minProduct = nums[0];
-        int result = nums[0];
-
-        for (int i = 1; i < nums.length; i++) {
-            if (nums[i] < 0) {
-                // Swap max and min if the current number is negative
-                int temp = maxProduct;
-                maxProduct = minProduct;
-                minProduct = temp;
+        int maxi=nums[0];
+        int mini=nums[0];
+        int result=nums[0];
+        for(int i=1;i<nums.length;i++){
+            if(nums[i]<0){
+                int temp=maxi;
+                maxi=mini;
+                mini=temp;
             }
-            
-            // Update maxProduct and minProduct
-            maxProduct = Math.max(nums[i], maxProduct * nums[i]);
-            minProduct = Math.min(nums[i], minProduct * nums[i]);
-            
-            // Update result
-            result = Math.max(result, maxProduct);
+            maxi=Math.max(nums[i],maxi*nums[i]);
+            mini=Math.min(nums[i],mini*nums[i]);
+            result=Math.max(result,maxi);
         }
-
-        return result;}
+        return result;
+    }
 }
