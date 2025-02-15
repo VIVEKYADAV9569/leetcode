742bc43d7a09675925e184fef7c5a1b53e1b68f6
VIVEKYADAV9569 <148001435+VIVEKYADAV9569@users.noreply.github.com>
2025-02-15 23:59:40 +0530

    Time: 0 ms, Memory: 41.1 MB - LeetHub

---
+++ 11980-257-100-same-tree/11980-257-100-same-tree.java
@@ -0,0 +1,10 @@
+
+class Solution {
+    // Vivek Yadav 100.Same Tree
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if(p==null&&q==null)return true;
+        if(p==null||q==null)return false;
+        if(p.val!=q.val)return false;
+        return isSameTree(p.left,q.left)&&isSameTree(p.right,q.right);
+    }
+}
